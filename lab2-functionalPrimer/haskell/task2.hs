2ca88dc2beda736dd97438e8d57044f5aef69b46
Will Giles <124457256+WillG06@users.noreply.github.com>
2026-01-28 15:37:55 +0000

    Changes 28/01

--- lab2-functionalPrimer/haskell/task2.hs
+++ lab2-functionalPrimer/haskell/task2.hs
@@ -2,3 +2,7 @@ main = do
     putStrLn "Welcome to the programme. Please enter your name"
     name <- getLine
     putStrLn("Hello " ++ name ++ ", hope you like Haskell.")
+    onePlusOne
+
+
+onePlusOne = putStrLn "1 +  1 = 2"
